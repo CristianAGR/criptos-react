2e838a0478aea01cf9fc18d05c8b57bfc40a7169
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-02-08 12:59:20 -0600

    añadir state y retornar valor

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -24,7 +24,7 @@ margin-top: 30px;
 
 const Formulario = () => {
 
-    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
 
   return (
     <form>
